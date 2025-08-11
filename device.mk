13eef27e9cf8d9379db26d14a50651943168e29f
Anouarbery <sirinaberriri@gmail.com>
2025-08-11 19:53:29 +0100

    Add files via upload

---
+++ device.mk
@@ -0,0 +1,72 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a56x
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Enable developer GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl-exynos \
+    android.hardware.boot@1.2-impl-exynos.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Recovery additional binaries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+
+# Shipping level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Vendor Boot Platform
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/platform/fstab.s5e8855:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.s5e8855 \
+    $(LOCAL_PATH)/prebuilt/platform/fstab.s5e8855:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/fstab.s5e8855 \
+    $(LOCAL_PATH)/prebuilt/platform/mgfx2.0_mid.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/lib/firmware/sgpu/mgfx2.0_mid.bin \
+    $(LOCAL_PATH)/prebuilt/platform/mgfx2.0_mid.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/firmware/sgpu/mgfx2.0_mid.bin \
+    $(LOCAL_PATH)/prebuilt/platform/gt9916k_a56x.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/vendor/firmware/tsp/gt9916k_a56x.bin
+
+# Decryption
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/platform/openrecoveryscript:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/cache/recovery/openrecoveryscript
---
+++ twrp_a56x.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from a56x device
+$(call inherit-product, device/samsung/a56x/device.mk)
+
+# Product Name
+PRODUCT_RELEASE_NAME := a56x
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A566B
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
